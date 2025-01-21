455a61c4b890bc7f2685d3a15713bb548e67fad5
Honzeenek <jan.palenik@icloud.com>
2025-01-21 20:27:17 +0100

    Added button arrow, that rotates when clicked. Button contact now shows dropdown menu. Added icons to the dropdown menu and gave them basic style. Javascript is functioning more, arrows, event listeners for the dropdown menu.

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ document.addEventListener('scroll', function() {
     const nav = document.querySelector('nav');
     if (window.scrollY > 170) { // Adjust the scroll value as needed
       nav.classList.add('nav-hidden');
+      dropdownContent.classList.remove("show");
     } else {
       nav.classList.remove('nav-hidden');
     }
@@ -14,3 +15,23 @@ document.getElementById('scroll-to-top').addEventListener('click', function(even
         behavior: 'smooth'
     });
 });
+
+const dropdownBtn = document.querySelector(".btn");
+const dropdownContent = document.querySelector(".dropdown-content");
+
+dropdownBtn.addEventListener("click", () => {
+    dropdownContent.classList.toggle("show");
+    arrow.classList.toggle('rotate');
+});
+
+window.addEventListener("click", (event) => {
+    if (!event.target.matches(".btn")) {
+        if (dropdownContent.classList.contains("show")) {
+            dropdownContent.classList.remove("show");
+            arrow.classList.remove('rotate');
+        }
+    }
+})
+
+const arrow = document.querySelector(".arrow");
+
